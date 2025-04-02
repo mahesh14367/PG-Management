be72952901ced0fe13e61da91ed1d78cdb7445bf
mahesh14367 <maheshkilaparthi14367@gmail.com>
2025-04-03 04:14:57 +0530

    Second change:PaymentRepositoryImpl:added userId as child in the Transactions directory

--- app/src/main/java/com/appsv/loginapp/login/data/repository/PaymentRepositoryImpl.kt
+++ app/src/main/java/com/appsv/loginapp/login/data/repository/PaymentRepositoryImpl.kt
@@ -3,12 +3,15 @@ package com.appsv.loginapp.login.data.repository
 import android.util.Log
 import com.appsv.loginapp.login.domain.model.Transaction
 import com.appsv.loginapp.login.domain.repository.PaymentRepository
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.tasks.await
 
 class PaymentRepositoryImpl:PaymentRepository {
 
-    private val db = FirebaseDatabase.getInstance().getReference("Transactions")
+    private val auth=FirebaseAuth.getInstance()
+
+    private val db = FirebaseDatabase.getInstance().getReference("Transactions").child("${auth.currentUser?.uid}")
 
     override suspend fun saveTransaction(transaction: Transaction): Result<Unit> {
         return try {
